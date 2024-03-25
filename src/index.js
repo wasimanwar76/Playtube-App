79dbc39af1775eced477bdfc1ccb81a5f020ae4a
Wasim Anwar <wasimsonu76@gmail.com>
2024-03-25 21:24:52 +0530

    Add all Require Express function in app.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: ".env",
 });
-const app = express();
 
-
-connectDB();
-app.listen(process.env.PORT_NO, () => {
-  console.log(`APP is listening on port no ${process.env.PORT_NO}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT_NO, () => {
+      console.log(`Server is listening on port no ${process.env.PORT_NO}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB db connection Failed |||", error);
+  });
